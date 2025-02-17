b96b51632c93bd1ec1de3b7cafaf1cc8f7340084
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-17 21:37:34 +0100

    feat: update mail notifier to use MailSender class

--- esp8266-notifier/src/main/java/com/feis/smarthouse/features/notifier/MailNotifier.java
+++ esp8266-notifier/src/main/java/com/feis/smarthouse/features/notifier/MailNotifier.java
@@ -1,18 +1,18 @@
 package com.feis.smarthouse.features.notifier;
 
+import com.feis.smarthouse.common.config.MailSender;
 import com.feis.smarthouse.features.notifier.interfaces.Notifier;
 
-import kotlin.NotImplementedError;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class MailNotifier implements Notifier {
-    private String recipient;
+    private MailSender mailSender;
 
     @Override
-    public void send() {
-        throw new NotImplementedError("Not yet implemented");
+    public void send(String recipient, String subject, String msg) throws Exception {
+        mailSender.deliverMessage(recipient, subject, msg, msg, false);
     }
 }
