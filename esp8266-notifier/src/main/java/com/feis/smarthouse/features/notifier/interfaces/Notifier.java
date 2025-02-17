d297d8c30d380ef07e02048631bc0a07c2fe488f
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-17 21:34:24 +0100

    feat: update notifier send method properties

--- esp8266-notifier/src/main/java/com/feis/smarthouse/features/notifier/interfaces/Notifier.java
+++ esp8266-notifier/src/main/java/com/feis/smarthouse/features/notifier/interfaces/Notifier.java
@@ -1,5 +1,9 @@
 package com.feis.smarthouse.features.notifier.interfaces;
 
 public interface Notifier {
-    void send();
+    // TODO: Update signature to throw custom app exception
+    // TODO: Add custom options map as method property (should allow
+    // useful options such as "allow answers" from a mail notifier implementation,
+    // etc...)
+    void send(String recipient, String subject, String msg) throws Exception;
 }
