2da2a0854b8899fb271ea070efa471a00f96282f
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-19 11:49:37 +0100

    feat: add triggered alarm handler

--- esp8266-notifier/src/main/java/com/feis/smarthouse/features/alarm/AlarmController.java
+++ esp8266-notifier/src/main/java/com/feis/smarthouse/features/alarm/AlarmController.java
@@ -1,16 +1,46 @@
 package com.feis.smarthouse.features.alarm;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.feis.smarthouse.common.dto.ResponseDto;
 import com.feis.smarthouse.common.interfaces.EndpointsRegister;
+import com.feis.smarthouse.common.util.Environment;
+import com.feis.smarthouse.features.notifier.MailNotifier;
+import com.feis.smarthouse.features.notifier.interfaces.Notifier;
 
 import io.javalin.Javalin;
-import kotlin.NotImplementedError;
+import io.javalin.http.Handler;
 
 public class AlarmController implements EndpointsRegister {
     @Override
     public void registerEndpoints(Javalin app) {
         // TODO: move "/api/v1" prefix outta here for reusability
-        app.get("/api/v1/alarm", (ctx) -> {
-            throw new NotImplementedError("Not yet implemented");
-        });
+        app.get("/api/v1/alarm", triggeredAlarm());
+    }
+
+    // TODO: REFACTOR
+    private Handler triggeredAlarm() {
+        return (ctx) -> {
+            // TODO: Make offset parsing dynamic based on client's tz
+            OffsetDateTime triggeredAt = Instant.now().atOffset(ZoneOffset.ofHours(1));
+            String formatted = triggeredAt.format(DateTimeFormatter.ofPattern("dd/MM/yyy 'at' kk:mm:ss O"));
+            List<Notifier> notifiers = new ArrayList<>(Arrays.asList(new MailNotifier()));
+            Map<String, String> notificationOptions = Environment
+                    .getEnvironmentVariables(Arrays.asList("EMAIL_NOTIFY_TO"));
+            for (Notifier notifier : notifiers) {
+                notifier.send(notificationOptions.get("EMAIL_NOTIFY_TO"), "Alarm Triggered",
+                        String.format("A motion has been detected on %s", formatted));
+            }
+            final int STATUS_CODE = 200;
+            ResponseDto<?> response = new ResponseDto<>(STATUS_CODE, "Notification sent", null);
+            ctx.status(STATUS_CODE).json(response);
+        };
     }
 }
