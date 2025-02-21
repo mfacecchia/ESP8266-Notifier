c931eab201b2c01c7cca707453a0a6c9bb7bac9c
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-21 19:39:58 +0100

    feat: add app builder configuration & startup in main app

--- esp8266-notifier/src/main/java/com/feis/smarthouse/App.java
+++ esp8266-notifier/src/main/java/com/feis/smarthouse/App.java
@@ -1,11 +1,13 @@
 package com.feis.smarthouse;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+
+import com.feis.smarthouse.common.config.AppBuilder;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        AppBuilder appBuilder = new AppBuilder(Arrays.asList());
+        appBuilder.configureEndpoints(true);
+        appBuilder.start();
     }
 }
