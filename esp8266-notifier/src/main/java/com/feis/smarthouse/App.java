43077b7bc50901bd01f1d71b402c1faeee7c35d7
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-21 19:41:00 +0100

    feat: add alarm controller kayer in main app

--- esp8266-notifier/src/main/java/com/feis/smarthouse/App.java
+++ esp8266-notifier/src/main/java/com/feis/smarthouse/App.java
@@ -3,10 +3,11 @@ package com.feis.smarthouse;
 import java.util.Arrays;
 
 import com.feis.smarthouse.common.config.AppBuilder;
+import com.feis.smarthouse.features.alarm.AlarmController;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        AppBuilder appBuilder = new AppBuilder(Arrays.asList());
+        AppBuilder appBuilder = new AppBuilder(Arrays.asList(new AlarmController()));
         appBuilder.configureEndpoints(true);
         appBuilder.start();
     }
