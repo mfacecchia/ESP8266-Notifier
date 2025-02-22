d95a84fdd83729209f570ba724c48bbe584279b8
Marco Facecchia <facecchiamarco@icloud.com>
2025-02-22 11:11:00 +0100

    refactor: replace variables from .env to System

--- src/main/java/com/feis/smarthouse/common/util/Environment.java
+++ src/main/java/com/feis/smarthouse/common/util/Environment.java
@@ -4,30 +4,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 /**
  * Utility class used to load load environmental variables
- * from a {@code .env} file.
  */
 public class Environment {
 
     private Environment() {
     }
 
-    /**
+    /*
      * Obtains environmental variables values from the list provided.
      * 
      * @param variablesName the list of all environmental variables key names to
-     *                      retrieve
+     * retrieve.
+     * 
      * @return a Map representing the env variables key-value pair
      */
     public static Map<String, String> getEnvironmentVariables(List<String> variablesName) {
-        // TODO: Use `System.getenv()` method
-        Dotenv dotenv = Dotenv.load();
         Map<String, String> env = new HashMap<>();
         for (String name : variablesName) {
-            env.put(name, dotenv.get(name));
+            env.put(name, System.getenv(name));
         }
         return env;
     }
